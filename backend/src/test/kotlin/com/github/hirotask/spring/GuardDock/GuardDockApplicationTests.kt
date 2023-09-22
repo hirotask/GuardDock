13c673fc1af98889b9899a4bb6485e5ec902b838
hirotask <hirotaskhashi@gmail.com>
2023-09-22 23:12:36 +0900

    [add]: springの追加

---
+++ backend/src/test/kotlin/com/github/hirotask/spring/GuardDock/GuardDockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.hirotask.spring.GuardDock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GuardDockApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
